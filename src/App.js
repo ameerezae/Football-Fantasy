3001c822f3d2d8787b39de1929d65700e1d53245
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-10-22 14:01:31 +0330

    Add redux precedure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import pickSquadContainer from "./containers/pickSquad/pickSquad";
+import {applyMiddleware, createStore} from "redux";
+import {Provider} from "react-redux";
+import promiseMiddleware from "redux-promise";
+import logger from "redux-logger";
+import rootReducer from "./reducers";
 
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
 
 class App extends Component {
     render() {
-        return(
+        return (
             <div>
-                <Router>
-                    <Switch>
-                        <Route path="/picksquad" component={pickSquadContainer}/>
-                    </Switch>
-                </Router>
-
+                <Provider store={createStoreWithMiddleware(rootReducer,applyMiddleware(logger))}>
+                    <Router>
+                        <Switch>
+                            <Route path="/picksquad" component={pickSquadContainer}/>
+                        </Switch>
+                    </Router>
+                </Provider>
             </div>
         )
     }
