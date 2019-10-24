2ea6410fb41718f17f8e02a0b1ce7157d07c3f79
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-10-24 16:30:41 +0330

    Add navbar and signIn route to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
+import  Navbar  from "./components/layout/Navbar"
 import React, { Component } from 'react';
-
+import SignIn from './components/auth/SignIn'
 export default class App extends Component {
   render() {
     return (
         <Router>
-          <switch>
-            
-          </switch>
-        </Router>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route path="/signin" component={SignIn} />
+            </Switch>
+            {/* <SignIn /> */}
+          </div>
+        </Router> 
       
     )
   }
