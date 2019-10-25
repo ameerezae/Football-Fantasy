527b9b5ace89c14fad36c9318a85d93104af8d7b
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-10-25 19:48:21 +0330

    Add signIn form

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux"
+import { userSignInRequest } from "../../_actions/authActions"
 
 class SignIn extends Component {
-    state = {
+    constructor(props)
+    {
+        super(props);
+    this.state = {
         email : '',
-        password :  ''
+        password1 :'',
     }
+
+    this.onChange = this.handleChange.bind(this);
+    this.onSubmit = this.handleSubmit.bind(this);
+}
     handleChange = (e) => {
         this.setState({
             [e.terget.id] : e.terget.value
@@ -12,13 +21,15 @@ class SignIn extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        // console.log(this.state)
+        this.props.userSignInRequest(this.state)
+
     }
     render() {
         return (
             <div className="main">
 
-            <section className="signup">
+            <section className="signin">
                 {/* <!-- <img src="images/signup-bg.jpg" alt=""> --> */}
                 <div className="container">
                         <form onSubmit={this.handleSubmit} id="signup-form" className="white">
@@ -26,14 +37,14 @@ class SignIn extends Component {
                             <br/> 
                             <div className="input-field">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" id="email" onChange={this.handleChange}/>
+                                <input type="email" id="email" value ={this.state.email} onChange={this.handleChange}/>
                             </div>
                             <div className="input-field">
-                            <label htmlFor="password">Password</label>
-                                <input type="password" id="password" onChange={this.handleChange}/>
+                            <label htmlFor="password1">Password</label>
+                                <input type="password" id="password1" value ={this.state.password1} onChange={this.handleChange}/>
                             </div>
                             <div className="input-field">
-                                <button className="btn pink lighten-1 z-depth-0">Sign IN</button>
+                                <button className="btn pink lighten-1 z-depth-0">LOGIN</button>
                             </div>
                         </form>
                 </div>
@@ -43,5 +54,8 @@ class SignIn extends Component {
         )
     }
 }
+const mapDispatchToProps = dispatch => ({
+    userSignInRequest: userInfo => dispatch(userSignInRequest(userInfo))
+  })
 
-export default SignIn
+export default  connect(null,mapDispatchToProps)(SignIn)
