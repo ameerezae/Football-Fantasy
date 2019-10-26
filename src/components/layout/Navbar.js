b2126b9e4ce2ad9a1e38d5c9bd096ed8d5db4d19
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-10-26 13:27:49 +0330

    Edit Navbar and Add mapstatetoprops

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import SignedInLinks from "./SignedInLinks"
-
+import { connect } from "react-redux"
 const Navbar = () => {
     return(
         <nav className="nav-wrapper pink ">
             <div className="container">
-                <Link to='/' className="brand-logo">sign in</Link>
+                <Link to='/' className="brand-logo">Football Fantasy</Link>
                 <SignedInLinks />
+
             </div>
         </nav>
     )
 }
+const mapStateToProps = (state) => {
+    console.log("salam",state);
+    return{
 
-export default Navbar
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
