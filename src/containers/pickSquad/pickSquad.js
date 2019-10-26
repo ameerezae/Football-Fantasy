ceb5eaf993f571ae6480e3a3d623267c05de2cab
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-10-26 15:47:33 +0330

    Add send data function and change positions name

--- src/containers/pickSquad/pickSquad.js
+++ src/containers/pickSquad/pickSquad.js
@@ -6,20 +6,72 @@ import PlayersTable from "./playersTable"
 import Anonymous from "../../assets/football-player.svg"
 import {AnimateOnChange} from "react-animation";
 import GoalK from "../../assets/GK.svg";
-import Bench from "../../assets/bench.svg"
-import {getWholeItems, setFilteredPosition, setPickedPosition, setPickedKey, toggleModal,setSquadName} from "../../actions";
+import Bench from "../../assets/bench.svg";
+import {Button} from "react-bootstrap"
+import {
+    getWholeItems,
+    setFilteredPosition,
+    setPickedPosition,
+    setPickedKey,
+    toggleModal,
+    setSquadName
+} from "../../actions";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import DetailsModal from "./detailsModal";
 import CountUp from "react-countup/build";
 import Swal from "sweetalert2";
+
 class PickSquadContainer extends Component {
     state = {
-        pickName : true,
+        pickName: true,
     };
 
     componentWillMount() {
         this.props.getWholeItems();
+    }
+
+    async handleSubmit(event, data) {
+        let changedData = data;
+        let picks = [];
+        Object.keys(data).forEach((key) => {
+            if (key === "Defender" || key === "Goalkeeper" || key === "Forward" || key === "Midfielder"){
+                const newData = data[key];
+                for(let i = 0 ; i< newData.length;i++){
+                    newData[i]["lineup"] = true;
+                    picks.push(newData[i]);
+                }
+            }else if(key === "bench"){
+                const newData = data[key];
+                for(let i = 0 ; i< newData.length;i++){
+                    newData[i]["lineup"] = false;
+                    picks.push(newData[i]);
+                }
+
+            }
+        });
+        for(let i = 0 ;i < picks.length;i++){
+            picks[i]["player_id"] = picks[i].id;
+        }
+        changedData["picks"] = picks;
+
+
+        try {
+            const url = "http://172.17.3.123:5000/team/pick-squad";
+            const token = localStorage.getItem("access_token")
+            const response = await fetch(`${url}`, {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization" : `Bearer ${token}`,
+                },
+                body : JSON.stringify(changedData)
+
+            })
+        } catch (e) {
+            console.log("SERVER DOWN")
+        }
     }
 
     onClick = (event) => {
@@ -45,21 +97,21 @@ class PickSquadContainer extends Component {
     };
 
     render() {
-        if(this.state.pickName){
+        if (this.state.pickName) {
             Swal.fire({
                 title: 'Enter your squad name :',
                 input: 'text',
-                confirmButtonText:"confirm",
+                confirmButtonText: "confirm",
                 inputValidator: (value) => {
                     this.props.setSquadName(value)
-                    this.setState({pickName:false})
+                    this.setState({pickName: false})
                 }
             })
         }
 
         const GK = (
-            this.props.format.forward ?
-                this.props.format.GK.map((element, key) => {
+            this.props.format.Forward ?
+                this.props.format.Goalkeeper.map((element, key) => {
                     return (
                         <div className="col" key={key}>
                             <div className="row justify-content-center">
@@ -67,10 +119,12 @@ class PickSquadContainer extends Component {
                                                  animationIn="bounceIn"
                                                  animationOut="bounceOut"
                                 >
-                                    {this.props.format.GK[key] ?
+                                    {this.props.format.Goalkeeper[key] ?
                                         <div className="container">
                                             <div className="row justify-content-center">
-                                                <img src={this.props.format.GK[key].image} onClick={this.pickedPlayerOnClick} name="GK" id={key} key={key}
+                                                <img src={this.props.format.Goalkeeper[key].image}
+                                                     onClick={this.pickedPlayerOnClick} name="Goalkeeper" id={key}
+                                                     key={key}
                                                      width="80px"
                                                      alt="pic"/>
                                             </div>
@@ -83,13 +137,13 @@ class PickSquadContainer extends Component {
                                                     paddingRight: "4px",
                                                     fontWeight: "200",
                                                     backgroundColor: "rgb(90, 247, 220)"
-                                                }}>{this.props.format.GK[key].name}</div>
+                                                }}>{this.props.format.Goalkeeper[key].name}</div>
                                             </div>
                                         </div> :
                                         <div className="container">
                                             <div className="row justify-content-center">
                                                 <img src={GoalK} onClick={this.onClick} alt="anonymous" width="80px"
-                                                     name="GK"
+                                                     name="Goalkeeper"
                                                      id={key} key={key}/>
                                             </div>
                                             <div className="row justify-content-center">
@@ -112,8 +166,8 @@ class PickSquadContainer extends Component {
         );
 
         const defenders = (
-            this.props.format.defender ?
-                this.props.format.defender.map((element, key) => {
+            this.props.format.Defender ?
+                this.props.format.Defender.map((element, key) => {
                     return (
                         <div className="col" key={key}>
                             <div className="row justify-content-center">
@@ -121,10 +175,12 @@ class PickSquadContainer extends Component {
                                     animationIn="bounceIn"
                                     animationOut="bounceOut"
                                 >
-                                    {this.props.format.defender[key] ?
+                                    {this.props.format.Defender[key] ?
                                         <div className="container">
                                             <div className="row justify-content-center">
-                                                <img src={this.props.format.defender[key].image} width="80px" onClick={this.pickedPlayerOnClick} name="defender" id={key} key={key}
+                                                <img src={this.props.format.Defender[key].image} width="80px"
+                                                     onClick={this.pickedPlayerOnClick} name="Defender" id={key}
+                                                     key={key}
                                                      alt="pic"/>
                                             </div>
                                             <div className="row mt-1 justify-content-center">
@@ -136,7 +192,7 @@ class PickSquadContainer extends Component {
                                                     paddingRight: "4px",
                                                     fontWeight: "200",
                                                     backgroundColor: "rgb(90, 247, 220)"
-                                                }}>{this.props.format.defender[key].name}</div>
+                                                }}>{this.props.format.Defender[key].name}</div>
                                             </div>
                                         </div>
 
@@ -144,7 +200,7 @@ class PickSquadContainer extends Component {
                                         <div className="container">
                                             <div className="row justify-content-center">
                                                 <img src={Anonymous} onClick={this.onClick} alt="anonymous" width="80px"
-                                                     name="defender"
+                                                     name="Defender"
                                                      id={key} key={key}/>
                                             </div>
                                             <div className="row justify-content-center">
@@ -168,8 +224,8 @@ class PickSquadContainer extends Component {
 
 
         const mids = (
-            this.props.format.middle ?
-                this.props.format.middle.map((element, key) => {
+            this.props.format.Midfielder ?
+                this.props.format.Midfielder.map((element, key) => {
                     return (
                         <div className="col" key={key}>
                             <div className="row justify-content-center">
@@ -177,10 +233,12 @@ class PickSquadContainer extends Component {
                                     animationIn="bounceIn"
                                     animationOut="bounceOut"
                                 >
-                                    {this.props.format.middle[key] ?
+                                    {this.props.format.Midfielder[key] ?
                                         <div className="container">
                                             <div className="row justify-content-center">
-                                                <img src={this.props.format.middle[key].image} width="80px" alt="pic" onClick={this.pickedPlayerOnClick} name="middle" id={key} key={key}/>
+                                                <img src={this.props.format.Midfielder[key].image} width="80px"
+                                                     alt="pic" onClick={this.pickedPlayerOnClick} name="Midfielder"
+                                                     id={key} key={key}/>
                                             </div>
                                             <div className="row mt-1 justify-content-center">
                                                 <div style={{
@@ -191,13 +249,13 @@ class PickSquadContainer extends Component {
                                                     paddingRight: "4px",
                                                     fontWeight: "200",
                                                     backgroundColor: "rgb(90, 247, 220)"
-                                                }}>{this.props.format.middle[key].name}</div>
+                                                }}>{this.props.format.Midfielder[key].name}</div>
                                             </div>
                                         </div> :
                                         <div className="container">
                                             <div className="row justify-content-center">
                                                 <img src={Anonymous} onClick={this.onClick} alt="anonymous" width="80px"
-                                                     name="middle"
+                                                     name="Midfielder"
                                                      id={key} key={key}/>
                                             </div>
                                             <div className="row justify-content-center">
@@ -220,8 +278,8 @@ class PickSquadContainer extends Component {
 
         );
         const forwards = (
-            this.props.format.forward ?
-                this.props.format.forward.map((element, key) => {
+            this.props.format.Forward ?
+                this.props.format.Forward.map((element, key) => {
                     return (
                         <div className="col" key={key}>
                             <div className="row justify-content-center">
@@ -229,10 +287,12 @@ class PickSquadContainer extends Component {
                                                  animationIn="bounceIn"
                                                  animationOut="bounceOut"
                                 >
-                                    {this.props.format.forward[key] ?
+                                    {this.props.format.Forward[key] ?
                                         <div className="container">
                                             <div className="row justify-content-center">
-                                                <img src={this.props.format.forward[key].image} width="80px" alt="pic" onClick={this.pickedPlayerOnClick} name="forward" id={key} key={key}/>
+                                                <img src={this.props.format.Forward[key].image} width="80px" alt="pic"
+                                                     onClick={this.pickedPlayerOnClick} name="Forward" id={key}
+                                                     key={key}/>
                                             </div>
                                             <div className="row mt-1 justify-content-center">
                                                 <div style={{
@@ -243,14 +303,14 @@ class PickSquadContainer extends Component {
                                                     paddingRight: "4px",
                                                     fontWeight: "200",
                                                     backgroundColor: "rgb(90, 247, 220)"
-                                                }}>{this.props.format.forward[key].name}</div>
+                                                }}>{this.props.format.Forward[key].name}</div>
                                             </div>
                                         </div>
                                         :
                                         <div className="container">
                                             <div className="row justify-content-center">
                                                 <img src={Anonymous} onClick={this.onClick} alt="anonymous" width="80px"
-                                                     name="forward"
+                                                     name="Forward"
                                                      id={key} key={key}/>
                                             </div>
                                             <div className="row justify-content-center">
@@ -273,7 +333,7 @@ class PickSquadContainer extends Component {
         );
 
         const bench = (
-            this.props.format.forward ?
+            this.props.format.Forward ?
                 this.props.format.bench.map((element, key) => {
                     return (
                         <div className="col" key={key}>
@@ -285,7 +345,8 @@ class PickSquadContainer extends Component {
                                     {this.props.format.bench[key] ?
                                         <div className="container">
                                             <div className="row justify-content-center">
-                                                <img src={this.props.format.bench[key].image} width="80px" onClick={this.pickedPlayerOnClick} name="bench" id={key} key={key}
+                                                <img src={this.props.format.bench[key].image} width="80px"
+                                                     onClick={this.pickedPlayerOnClick} name="bench" id={key} key={key}
                                                      alt="pic"/>
                                             </div>
                                             <div className="row justify-content-center">
@@ -337,12 +398,23 @@ class PickSquadContainer extends Component {
                         </div>
                         <div className="col-lg-7">
                             <div className="row align-items-center mt-2">
-                                <h4 className="text-white ml-3">Pick your squad</h4>
-                                <h5 className="text-white ml-5">Budget: </h5>
-                                <h5 className="text-white pb-0 ml-2"><CountUp start={50} end={this.props.format.money} duration = {1}
-                                                                         separator = "," suffix="$" /></h5>
+                                <div className="col-10">
+                                    <div className="row">
+                                        <h4 className="text-white ml-3">Pick your squad</h4>
+                                        <h5 className="text-white ml-5">Budget: </h5>
+                                        <h5 className="text-white pb-0 ml-2"><CountUp start={50} end={this.props.format.budget}
+                                                                                      duration={1}
+                                                                                      separator="," suffix="$"/></h5>
+                                    </div>
+                                </div>
+                                <div className="col-2">
+                                    <div className="row justify-content-end">
+                                        <Button variant="primary" size="md" onClick={(event) => this.handleSubmit(event, this.props.format)}>Confirm</Button>
+                                    </div>
+                                </div>
+
                             </div>
-                            <hr style={{background:"white"}}/>
+                            <hr style={{background: "white"}}/>
                             <DetailsModal/>
                             <div className="row align-items-center">
                                 <div className="container-fluid field-background padding-to-field">
