b5fd0f6065593464c44f25a11fe77be0c86b7648
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-10-28 17:02:03 +0330

    Edit login signup actions and add async method

--- src/_actions/authActions.js
+++ src/_actions/authActions.js
@@ -1,60 +1,27 @@
 // import React from 'react'
 // import axios from 'axios'
+import Login_page_api from "../_api/authApi"
 export const auth_action_types = {
   LOGIN_SUCCESS : 'LOGIN_SUCCESS',
   LOGIN_FAILURE : 'LOGIN_FAILURE',
   LOGOUT_SUCCESS : 'LOGOUT_SUCCESS'
 }
 
-export const userSignUpRequest = user => {
-    return dispatch => {
-        return fetch("http://172.17.3.123:5000/auth/registeration",{
-            method: "POST",
-            mode: "cors",
-            headers: {
-              'Content-Type': 'application/json',
-              // Accept: 'application/json',
-              // mode: "CORS",
-            },
-            body: JSON.stringify(
-              user
-            )
-          })
-            .then(resp => resp.json())
-            // .catch(err => )
-            .then(data => {
-              if (data.message) {
-                console.log(data.message)
-                localStorage.setItem("access_token", data.access_token)
-                localStorage.setItem("refresh_token", data.refresh_token)
-                dispatch(loginUser(data))
-              } 
-            })
-    }
+export const userSignInRequest = (credentials) => {
+  return async function(dispatch){
+      let response = await Login_page_api.login(credentials)
+      localStorage.setItem("access_token", response.data.access_token)
+      localStorage.setItem("refresh_token", response.data.refresh_token)
+      dispatch(loginUser(response))
+  }
 }
 
-export const userSignInRequest = user => {
-  console.log(user)
-  return dispatch => {
-      return fetch("http://172.17.3.123:5000/auth/login", {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            'Content-Type': 'application/json',
-            // Accept: 'application/json',
-          },
-          body: JSON.stringify(user)
-        })
-          .then(resp => resp.json())
-          .then(data => {
-            if (data.message) {
-              console.log(data)
-              localStorage.setItem("access_token", data.access_token)
-              localStorage.setItem("refresh_token", data.refresh_token)
-              // console.log("pashmam")
-              dispatch(loginUser(data))
-            }
-          })
+export const userSignUpRequest = (credentials) => {
+  return async function(dispatch){
+      let response = await Login_page_api.signup(credentials) 
+      localStorage.setItem("access_token", response.data.access_token)
+      localStorage.setItem("refresh_token", response.data.refresh_token)
+      dispatch(loginUser(response))
   }
 }
 
