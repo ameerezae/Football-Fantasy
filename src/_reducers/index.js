e5b1671dcc79bf373a5e594863b00a1500a39601
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-10-28 14:09:46 +0330

    Add formation reducers to root reducer

--- src/_reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "redux";
-import formatReducer from "./formatReducer";
-const rootReducer = combineReducers({
-    formatReducer,
-
-});
-export default rootReducer;
--- src/_reducers/rootReducer.js
+++ src/_reducers/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
-
+import formatReducer from "./formatReducer";
 import { authReducer } from './authReducer';
 
 const rootReducer = combineReducers({
-    auth : authReducer
+    auth : authReducer,
+    format : formatReducer,
 });
 
 export default rootReducer;
