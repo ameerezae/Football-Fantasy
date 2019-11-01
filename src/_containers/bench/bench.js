7b0fa5ffb643d17bf37bbf16db68920f433d1bf5
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 11:24:05 +0330

    Add bench class

---
+++ src/_containers/bench/bench.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Player from "../player/player";
+class Bench extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Bench;
