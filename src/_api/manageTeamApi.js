a1b410c3371a39f662fcc9e04eeba989efb9c4a1
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 11:57:05 +0330

    Add getMyTeam function

---
+++ src/_api/manageTeamApi.js
@@ -0,0 +1,24 @@
+import * as api_urls from "./api_urls";
+
+
+class ManageTeamApi{
+    static async getMyTeam(){
+        const token = localStorage.getItem("access_token");
+        const config = {
+            mode: "cors",
+            headers:
+                {
+                    'Content-Type': 'application/json',
+                    "Authorization" : `Bearer ${token}`
+                }
+        }
+
+        const response = await axios.get(
+            api_urls.MANAGE_TEAM,
+            config
+        );
+        return response;
+    }
+}
+
+export default ManageTeamApi;
