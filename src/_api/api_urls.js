3d24095ad80b6d8098b293f8edcc113a0b99d41e
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 11:56:38 +0330

    Add manageTeam api url

--- src/_api/api_urls.js
+++ src/_api/api_urls.js
@@ -2,3 +2,4 @@ export const MAIN = "http://172.17.3.123:5000"
 export const LOGIN = MAIN + "/auth/login"
 export const SIGNUP = MAIN + "/auth/registeration"
 export const PICKSQUAD = MAIN + "/team/pick-squad"
+export const MANAGE_TEAM = MAIN + "/team/my-team";
