fac4c091b23e13c067915c2605d655a79f79a0a7
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 21:55:11 +0330

    Add substitution function

--- src/_containers/squad/squad.js
+++ src/_containers/squad/squad.js
@@ -2,70 +2,92 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import Player from "../player/player";
 import {bindActionCreators} from "redux";
-import {setFirstSelected} from "../../_actions/manageTeamActions";
+import {setFirstSelected, setMyNewTeam} from "../../_actions/manageTeamActions";
 
 class Squad extends Component {
 
+    substitution = (secondSelectedKey) => {
+        let newTeam = this.props.myTeam.squad;
+        newTeam[this.props.myTeam.firstSelected].lineup = false;
+        newTeam[secondSelectedKey].lineup = true;
+        this.props.setMyNewTeam(newTeam);
+    };
 
     render() {
         const sqaud = (
             <div>
                 <div className="row justify-content-center">
-                    {this.props.myTeam.squad?
-                        this.props.myTeam.squad.map((element,key)=>{
-                        if(element.position === "Goalkeeper" && element.lineup){
-                            return(
-                                <div  className="col">
-                                    <div onClick={!this.props.myTeam.firstSelected ? ()=>{this.props.setFirstSelected(key)}:()=>{console.log(key)}}>
-                                        <Player number={key} info={element}/>
-                                    </div>
+                    {this.props.myTeam.squad ?
+                        this.props.myTeam.squad.map((element, key) => {
+                            if (element.position === "Goalkeeper" && element.lineup) {
+                                return (
+                                    <div className="col">
+                                        <div onClick={!this.props.myTeam.firstSelected ? () => {
+                                            this.props.setFirstSelected(key)
+                                        } : () => {
+                                            this.substitution(key)
+                                        }}>
+                                            <Player number={key} info={element}/>
+                                        </div>
 
-                                </div>
-                            )
-                        }
-                    }):null}
-                </div>
-                <div className="row justify-content-center">
-                    {this.props.myTeam.squad?
-                        this.props.myTeam.squad.map((element,key)=>{
-                        if(element.position === "Defender" && element.lineup){
-                            return(
-                                <div className="col">
-                                    <div onClick={()=>{this.props.setFirstSelected(key)}}>
-                                        <Player number={key} info={element}/>
                                     </div>
-                                </div>
-                            )
-                        }
-                    }):null}
+                                )
+                            }
+                        }) : null}
                 </div>
                 <div className="row justify-content-center">
-                    {this.props.myTeam.squad?
-                        this.props.myTeam.squad.map((element,key)=>{
-                            if(element.position === "Midfielder" && element.lineup){
-                                return(
+                    {this.props.myTeam.squad ?
+                        this.props.myTeam.squad.map((element, key) => {
+                            if (element.position === "Defender" && element.lineup) {
+                                return (
                                     <div className="col">
-                                        <div onClick={()=>{this.props.setFirstSelected(key)}}>
+                                        <div onClick={!this.props.myTeam.firstSelected ? () => {
+                                            this.props.setFirstSelected(key)
+                                        } : () => {
+                                            this.substitution(key)
+                                        }}>
                                             <Player number={key} info={element}/>
                                         </div>
                                     </div>
                                 )
                             }
-                        }):null}
+                        }) : null}
                 </div>
                 <div className="row justify-content-center">
-                    {this.props.myTeam.squad?
-                        this.props.myTeam.squad.map((element,key)=>{
-                            if(element.position === "Forward" && element.lineup){
-                                return(
+                    {this.props.myTeam.squad ?
+                        this.props.myTeam.squad.map((element, key) => {
+                            if (element.position === "Midfielder" && element.lineup) {
+                                return (
                                     <div className="col">
-                                        <div onClick={()=>{this.props.setFirstSelected(key)}}>
+                                        <div onClick={!this.props.myTeam.firstSelected ? () => {
+                                            this.props.setFirstSelected(key)
+                                        } : () => {
+                                            this.substitution(key)
+                                        }}>
                                             <Player number={key} info={element}/>
                                         </div>
                                     </div>
                                 )
                             }
-                        }):null}
+                        }) : null}
+                </div>
+                <div className="row justify-content-center">
+                    {this.props.myTeam.squad ?
+                        this.props.myTeam.squad.map((element, key) => {
+                            if (element.position === "Forward" && element.lineup) {
+                                return (
+                                    <div className="col">
+                                        <div onClick={!this.props.myTeam.firstSelected ? () => {
+                                            this.props.setFirstSelected(key)
+                                        } : () => {
+                                            this.substitution(key)
+                                        }}>
+                                            <Player number={key} info={element}/>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        }) : null}
                 </div>
             </div>
         )
@@ -85,10 +107,11 @@ function mapStateToProps(state) {
     }
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
     return bindActionCreators({
         setFirstSelected,
-    },dispatch)
+        setMyNewTeam
+    }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Squad);
