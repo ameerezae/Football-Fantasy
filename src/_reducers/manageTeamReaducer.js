d907f9c2301a70f8ade712d0f09a203989f3e388
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 20:50:39 +0330

    Add firstSelectd item to state

--- src/_reducers/manageTeamReaducer.js
+++ src/_reducers/manageTeamReaducer.js
@@ -2,8 +2,13 @@ import * as types from "../_actions/types";
 
 export default function (state = {}, action) {
     switch (action.type) {
-        case types.manageTeam_action_types.GET_MY_TEAM_SUCCESS : return{...state, squad : action.payload};
+        case types.manageTeam_action_types.GET_MY_TEAM_SUCCESS :
+            return {...state, squad: action.payload};
 
-        default : return state;
+        case types.manageTeam_action_types.SET_FIRST_SELECTED_SUCCESS :
+            return {...state, firstSelected: action.payload};
+
+        default :
+            return state;
     }
 }
