b9cfb26b1b9401de800023b1e5c8c1feac202a22
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 13:03:24 +0330

    Edit render function

--- src/_containers/player/player.js
+++ src/_containers/player/player.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
-
+import "./player.scss";
 class Player extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="container">
+                <div className="row justify-content-center">
+                    <img src={this.props.image} width="80" alt="pic"
+                         name={this.props.position} id={this.props.id}
+                    />
+                </div>
+                <div className="row mt-1 justify-content-center">
+                    <div className="player-name-style">{this.props.name}</div>
+                </div>
             </div>
         );
     }
