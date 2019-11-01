3add06946132e1d94735689149332c50531d9315
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 11:24:35 +0330

    Add manageTeam class

---
+++ src/_containers/manageTeam/manageTeam.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Substitution from "../substitution/substitution";
+class ManageTeam extends Component {
+    render() {
+        return (
+            <Substitution/>
+        );
+    }
+}
+
+export default ManageTeam;
