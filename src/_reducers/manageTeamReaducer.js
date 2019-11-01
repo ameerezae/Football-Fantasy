6d8fec6ee6699e12c882f0cb9fbb4b559462de22
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 18:22:15 +0330

    Edit state

--- src/_reducers/manageTeamReaducer.js
+++ src/_reducers/manageTeamReaducer.js
@@ -2,7 +2,7 @@ import * as types from "../_actions/types";
 
 export default function (state = {}, action) {
     switch (action.type) {
-        case types.manageTeam_action_types.GET_MY_TEAM_SUCCESS : return{...state, myTeam : action.payload};
+        case types.manageTeam_action_types.GET_MY_TEAM_SUCCESS : return{...state, squad : action.payload};
 
         default : return state;
     }
