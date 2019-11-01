e929f2d4528c7af6d6f4f7a0556e6e25486fa27c
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 11:22:14 +0330

    Add Substitution class

---
+++ src/_containers/substitution/substitution.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Squad from "../squad/squad";
+import Bench from "../bench/bench";
+class Substitution extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Substitution;
