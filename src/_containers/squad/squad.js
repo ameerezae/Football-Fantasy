e9d7af1f9f5fda03afbcdf405a6e32981b12e04c
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 11:25:20 +0330

    Add squad class

---
+++ src/_containers/squad/squad.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Player from "../player/player";
+class Squad extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Squad;
