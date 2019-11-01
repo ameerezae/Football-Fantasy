02f8fdd5a1b4e4d9e7a6409981f5e73323e35fc7
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 19:21:03 +0330

    Add bench player

--- src/_containers/bench/bench.js
+++ src/_containers/bench/bench.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
 import Player from "../player/player";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+
 class Bench extends Component {
     render() {
+        const bench = (
+            <div className="row justify-content-center">
+                {this.props.myTeam.squad?
+                    this.props.myTeam.squad.map((element,key)=>{
+                        if(!element.lineup){
+                            return(
+                                <div className="col">
+                                    <Player number={key} info={element}/>
+                                    {key}
+                                </div>
+                            )
+                        }
+                    }):null}
+            </div>
+        )
         return (
             <div>
-
+                {bench}
             </div>
         );
     }
 }
 
-export default Bench;
+function mapStateToProps(state){
+    return{
+        myTeam : state.manageTeamReaducer
+    }
+}
+
+export default connect(mapStateToProps,null)(Bench);
