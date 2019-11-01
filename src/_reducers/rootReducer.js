a6e54f5afe667f3e69f71b30e63fcaec2c2f7079
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 12:27:26 +0330

    Edit root reducer

--- src/_reducers/rootReducer.js
+++ src/_reducers/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import formatReducer from "./formatReducer";
 import { authReducer } from './authReducer';
-
+import manageTeamReaducer from "./manageTeamReaducer";
 const rootReducer = combineReducers({
     authReducer,
     formatReducer,
+    manageTeamReaducer
 });
 
 export default rootReducer;
