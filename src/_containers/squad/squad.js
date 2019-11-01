23708b2c45f61958db223bd4013baec2b74f9d5b
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 19:22:15 +0330

    Add squad players

--- src/_containers/squad/squad.js
+++ src/_containers/squad/squad.js
@@ -1,13 +1,81 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 import Player from "../player/player";
-class Squad extends Component {
-    render() {
-        return (
-            <div>
 
+class Squad extends Component {
+
+
+    render() {
+        const sqaud = (
+            <div>
+                <div className="row justify-content-center">
+                    {this.props.myTeam.squad?
+                        this.props.myTeam.squad.map((element,key)=>{
+                        if(element.position === "Goalkeeper" && element.lineup){
+                            return(
+                                <div className="col">
+                                    <Player number={key} info={element}/>
+                                    {key}
+                                </div>
+                            )
+                        }
+                    }):null}
+                </div>
+                <div className="row justify-content-center">
+                    {this.props.myTeam.squad?
+                        this.props.myTeam.squad.map((element,key)=>{
+                        if(element.position === "Defender" && element.lineup){
+                            return(
+                                <div className="col">
+                                    <Player number={key} info={element}/>
+                                    {key}
+                                </div>
+                            )
+                        }
+                    }):null}
+                </div>
+                <div className="row justify-content-center">
+                    {this.props.myTeam.squad?
+                        this.props.myTeam.squad.map((element,key)=>{
+                            if(element.position === "Midfielder" && element.lineup){
+                                return(
+                                    <div className="col">
+                                        <Player number={key} info={element}/>
+                                        {key}
+                                    </div>
+                                )
+                            }
+                        }):null}
+                </div>
+                <div className="row justify-content-center">
+                    {this.props.myTeam.squad?
+                        this.props.myTeam.squad.map((element,key)=>{
+                            if(element.position === "Forward" && element.lineup){
+                                return(
+                                    <div className="col">
+                                        <Player number={key} info={element}/>
+                                        {key}
+                                    </div>
+                                )
+                            }
+                        }):null}
+                </div>
+            </div>
+        )
+
+        return (
+            <div className="container-fluid field-background">
+                {sqaud}
             </div>
         );
     }
 }
 
-export default Squad;
+
+function mapStateToProps(state) {
+    return {
+        myTeam: state.manageTeamReaducer,
+    }
+}
+
+export default connect(mapStateToProps, null)(Squad);
