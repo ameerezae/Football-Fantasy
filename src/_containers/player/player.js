8cc2f12f111e659bf9333f05828f1fcb2b254671
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 19:21:45 +0330

    Add player info

--- src/_containers/player/player.js
+++ src/_containers/player/player.js
@@ -2,15 +2,16 @@ import React, {Component} from 'react';
 import "./player.scss";
 class Player extends Component {
     render() {
+
         return (
-            <div className="container">
+            <div onClick={()=>{console.log(this.props.number)}} className="container">
                 <div className="row justify-content-center">
-                    <img src={this.props.image} width="80" alt="pic"
-                         name={this.props.position} id={this.props.id}
+                    <img src={this.props.info.image} width="80" alt="pic"
+                         name={this.props.info.position} id={this.props.info.id}
                     />
                 </div>
                 <div className="row mt-1 justify-content-center">
-                    <div className="player-name-style">{this.props.name}</div>
+                    <div className="player-name-style">{this.props.info.name}</div>
                 </div>
             </div>
         );
