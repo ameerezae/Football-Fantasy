01951fecce7ee45afe2514ec5a0d81a8996bfcd0
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-01 19:23:45 +0330

    Add bench players

--- src/_containers/substitution/substitution.js
+++ src/_containers/substitution/substitution.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Squad from "../squad/squad";
 import Bench from "../bench/bench";
+
 class Substitution extends Component {
     render() {
         return (
             <div>
-
+                <Squad/>
+                <Bench/>
             </div>
         );
     }
