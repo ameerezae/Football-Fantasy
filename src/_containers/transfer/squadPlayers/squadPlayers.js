c56ad22ce47224dbcc50929d40533d22a367a8b1
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-04 22:02:18 +0330

    Edit render function

--- src/_containers/transfer/squadPlayers/squadPlayers.js
+++ src/_containers/transfer/squadPlayers/squadPlayers.js
@@ -5,37 +5,60 @@ import {bindActionCreators} from "redux";
 import {IoIosInformationCircle} from "react-icons/io";
 import List, {ListItem, ListItemText, ListItemGraphic, ListItemMeta,} from '@material/react-list';
 import {getMyTeamForTransfer} from "../../../_actions/manageTeamActions";
+import {setFirstSelected, enableTransferTable} from "../../../_actions/manageTeamActions";
 
 class SquadPlayers extends Component {
     componentWillMount() {
         this.props.getMyTeamForTransfer();
     }
 
+    // check_2_5_5_3 = (team) => {
+    //     let gkCounter = 0, defCounter = 0, midCounter = 0, forwardCounter = 0;
+    //     team.forEach((element, key) => {
+    //         switch (element.position) {
+    //             case "goalKeeper" : gkCounter++;break;
+    //             case "Defender" : defCounter++;break;
+    //             case "Midfielder" : midCounter++;break;
+    //             case "Forward" : forwardCounter++;break;
+    //             default : break;
+    //         }
+    //     });
+    //
+    //     return !(gkCounter > 2 || defCounter > 5 || midCounter > 5 || forwardCounter > 3);
+    // };
+
+
     render() {
         return (
-            <List twoLine
-                  handleSelect={(activatedItemIndex) => console.log(activatedItemIndex)} dense>
+            <div>
+                <List twoLine
+                      handleSelect={(activatedItemIndex) => {
+                          this.props.setFirstSelected(activatedItemIndex);
+                          this.props.enableTransferTable(true)
+                      }} dense>
 
-                {this.props.myTeam.myTeamForTransfer? this.props.myTeam.myTeamForTransfer.map((element,key)=>{
-                    return(
-                        <ListItem key={key} className="text-white">
-                            <ListItemGraphic className="list-image" graphic={<img src={element.image} alt="sd"/>}/>
-                            <ListItemText
-                                className="text-white"
-                                primaryText={element.name.slice(0, 10)}
-                                secondaryText={element.club}/>
-                            <ListItemText
-                                className="whiteText ml-3"
-                                primaryText={element.price}
-                                secondaryText={element.position}/>
-                            <ListItemMeta style={{color: "white", fontSize: "1.5rem", verticalAlign: "center"}}
-                                          meta={<IoIosInformationCircle/>}/>
-                        </ListItem>
-                    )
-                }):null}
+                    {this.props.myTeam.myTeamForTransfer ? this.props.myTeam.myTeamForTransfer.map((element, key) => {
+                        return (
+                            <ListItem key={key} className="text-white">
+                                <ListItemGraphic className="list-image" graphic={<img src={element.image} alt="sd"/>}/>
+                                <ListItemText
+                                    className="text-white"
+                                    primaryText={element.name.slice(0, 10)}
+                                    secondaryText={element.club}/>
+                                <ListItemText
+                                    className="whiteText ml-3"
+                                    primaryText={element.price}
+                                    secondaryText={element.position}/>
+                                <ListItemMeta style={{color: "white", fontSize: "1.5rem", verticalAlign: "center"}}
+                                              meta={<IoIosInformationCircle/>}/>
+                            </ListItem>
+                        )
+                    }) : null}
 
 
-            </List>
+                </List>
+
+            </div>
 
         )
     }
@@ -50,6 +73,8 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
     return bindActionCreators({
         getMyTeamForTransfer,
+        setFirstSelected,
+        enableTransferTable
     }, dispatch)
 }
 
