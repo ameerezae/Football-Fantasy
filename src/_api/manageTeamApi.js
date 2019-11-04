e5b0d92124867f1332b250b058a81e51466b8b99
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-05 00:41:25 +0330

    Add post transfered player api

--- src/_api/manageTeamApi.js
+++ src/_api/manageTeamApi.js
@@ -1,15 +1,15 @@
 import * as api_urls from "./api_urls";
 import axios from "axios";
 
-class ManageTeamApi{
-    static async getMyTeam(){
+class ManageTeamApi {
+    static async getMyTeam() {
         const token = localStorage.getItem("access_token");
         const config = {
             mode: "cors",
             headers:
                 {
                     'Content-Type': 'application/json',
-                    "Authorization" : `Bearer ${token}`
+                    "Authorization": `Bearer ${token}`
                 }
         }
 
@@ -20,18 +20,50 @@ class ManageTeamApi{
         return response;
     }
 
-    static async getTransferablePlayers(){
+    static async getTransferablePlayers() {
         const token = localStorage.getItem("access_token");
         const config = {
             mode: "cors",
             headers:
                 {
                     'Content-Type': 'application/json',
-                    "Authorization" : `Bearer ${token}`
+                    "Authorization": `Bearer ${token}`
                 }
         }
         const response = await axios.get(
             api_urls.GET_TRANSFERABLE,
+            config
+        );
+        return response;
+    }
+
+
+    static async sendTransferedPlayer(playerIn, playerOut) {
+        const token = localStorage.getItem("access_token");
+        const config =
+            {
+                mode: "cors",
+                headers:
+                    {
+                        'Content-Type': 'application/json',
+                        "Authorization": `Bearer ${token}`
+                    }
+            };
+           const body = JSON.stringify(
+               {
+                   "player_in": {
+                       "id" : playerIn.id,
+                       "name" : playerIn.name
+                   },
+                   "player_out": {
+                       "id" : playerOut.id,
+                       "name" : playerOut.name
+                   }
+               }
+           );
+        const response = axios.post(
+            api_urls.POST_TRANSFERABLE,
+            body,
             config
         );
         return response;
