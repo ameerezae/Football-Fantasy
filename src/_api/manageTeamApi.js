29a86c582226df2e443a2f3144950857b492b489
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-04 15:46:04 +0330

    Add getTransferable api

--- src/_api/manageTeamApi.js
+++ src/_api/manageTeamApi.js
@@ -19,6 +19,23 @@ class ManageTeamApi{
         );
         return response;
     }
+
+    static async getTransferablePlayers(){
+        const token = localStorage.getItem("access_token");
+        const config = {
+            mode: "cors",
+            headers:
+                {
+                    'Content-Type': 'application/json',
+                    "Authorization" : `Bearer ${token}`
+                }
+        }
+        const response = await axios.get(
+            api_urls.GET_TRANSFERABLE,
+            config
+        );
+        return response;
+    }
 }
 
 export default ManageTeamApi;
