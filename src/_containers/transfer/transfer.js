cbb1c4504ad13709b3aecc05e5b2407e39b9032e
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-04 23:23:01 +0330

    Add transfer button

--- src/_containers/transfer/transfer.js
+++ src/_containers/transfer/transfer.js
@@ -3,17 +3,25 @@ import SquadPlayers from "./squadPlayers/squadPlayers";
 import AllPlayers from "./allPlayers/allPlayers";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
-import {getMyTeamForTransfer} from "../../_actions/manageTeamActions";
+import {Button} from "react-bootstrap";
 
 class Transfer extends Component {
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-6">
+                    <div className="col-5">
                         <SquadPlayers/>
                     </div>
-                    <div className="col-6">
+                    <div className="col-2">
+                        <Button variant="primary"
+                                disabled={!((this.props.myTeam.secondSelectedTransfer ||
+                                    this.props.myTeam.secondSelectedTransfer === 0) && (
+                                    this.props.myTeam.firstSelected ||
+                                    this.props.myTeam.firstSelected === 0))}
+                        onClick={}>Transfer</Button>
+                    </div>
+                    <div className="col-5">
                         <AllPlayers/>
                     </div>
                 </div>
@@ -29,8 +37,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-    },dispatch)
+    return bindActionCreators({}, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Transfer);
