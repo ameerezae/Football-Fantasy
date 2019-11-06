d89b7a4bc42f66f09e24984d15ec5a46f929754d
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-06 22:17:05 +0330

    Add tabs to the manageteam page

--- src/_containers/manageTeam/manageTeam.js
+++ src/_containers/manageTeam/manageTeam.js
@@ -1,16 +1,49 @@
 import React, {Component} from 'react';
 import Substitution from "./substitution/substitution";
-import {getMyTeam} from "../../_actions/manageTeamActions";
-import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
+import Transfer from "../transfer/transfer";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+import ThreePlayersImage from "../../_assets/player-comp-3-1x.7eb15f60.png"
+import "./manageTeam.scss";
 class ManageTeam extends Component {
-    componentWillMount() {
-        this.props.getMyTeam();
-    }
+
 
     render() {
         return (
-            <Substitution/>
+            <div className="container-fluid">
+                <div className="container">
+                    <div className="row">
+                        <div className="col row align-items-center">
+                                <h1 className="text-white">FANTASY</h1>
+                        </div>
+                        <div className="col">
+                            <div className="row justify-content-end">
+                                <img src={ThreePlayersImage} width="250" alt="ww"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="main-background"></div>
+                    <Tabs>
+                        <TabList>
+                            <Tab>Substitution</Tab>
+                            <Tab>Transfer</Tab>
+                        </TabList>
+                        <TabPanel>
+                            <Substitution/>
+                        </TabPanel>
+                        <TabPanel>
+                            <Transfer/>
+                        </TabPanel>
+                    </Tabs>
+                </div>
+            </div>
+
+
+
+
         );
     }
 }
@@ -21,10 +54,6 @@ function mapStateToProps(state){
     }
 }
 
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        getMyTeam,
-    },dispatch)
-}
 
-export default connect(mapStateToProps,mapDispatchToProps)(ManageTeam);
+
+export default connect(mapStateToProps,null)(ManageTeam);
