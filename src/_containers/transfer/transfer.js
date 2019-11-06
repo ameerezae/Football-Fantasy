ec9f5df75c7f4d62f25c9541ece615931ece66ed
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-06 23:53:28 +0330

    Add restore button and animation

--- src/_containers/transfer/transfer.js
+++ src/_containers/transfer/transfer.js
@@ -7,6 +7,13 @@ import {Button} from "react-bootstrap";
 import ManageTeamApi from "../../_api/manageTeamApi";
 import '@lottiefiles/lottie-player';
 import AnonymousPlayer from "../../_assets/football-player.svg";
+import {
+    selectSecondTransfer,
+    selectFirstTransfer,
+    enableTransferTable,
+    isAllowedToTransfer
+} from "../../_actions/manageTeamActions";
+import {AnimateOnChange} from "react-animation";
 
 
 class Transfer extends Component {
@@ -17,96 +24,133 @@ class Transfer extends Component {
                     <div className="col">
                         <div className="container">
                             <div className="row justify-content-center">
-                                {this.props.myTeam.firstSelectedTransfer || this.props.myTeam.firstSelectedTransfer === 0 ?
-                                    <div>
-                                        <div className="row justify-content-center">
-                                            <img width="150"
-                                                 src={this.props.myTeam.myTeamForTransfer[this.props.myTeam.firstSelectedTransfer].image}/>
+                                <AnimateOnChange
+                                    animationIn="bounceIn"
+                                    animationOut="bounceOut"
+                                >
+                                    {this.props.myTeam.firstSelectedTransfer || this.props.myTeam.firstSelectedTransfer === 0 ?
+                                        <div>
+                                            <div className="row justify-content-center">
+                                                <img width="150"
+                                                     src={this.props.myTeam.myTeamForTransfer[this.props.myTeam.firstSelectedTransfer].image}/>
+                                            </div>
+                                            <div className="row justify-content-center">
+                                                <div
+                                                    className="text-white">{this.props.myTeam.myTeamForTransfer[this.props.myTeam.firstSelectedTransfer].name}</div>
+                                            </div>
+                                            <div className="row justify-content-center">
+                                                <div
+                                                    className="text-white">{this.props.myTeam.myTeamForTransfer[this.props.myTeam.firstSelectedTransfer].position}</div>
+                                            </div>
                                         </div>
-                                        <div className="row justify-content-center">
-                                            <div className="text-white">{this.props.myTeam.myTeamForTransfer[this.props.myTeam.firstSelectedTransfer].name}</div>
-                                        </div>
-                                        <div className="row justify-content-center">
-                                            <div className="text-white">{this.props.myTeam.myTeamForTransfer[this.props.myTeam.firstSelectedTransfer].position}</div>
-                                        </div>
-                                    </div>
 
-                                    :
-                                    <div>
-                                        <div className="row justify-content-center">
-                                            <img width="150"
-                                                 src={AnonymousPlayer}/>
-                                        </div>
-                                        <div className="row justify-content-center">
-                                            <div>&nbsp;</div>
-                                        </div>
-                                        <div className="row justify-content-center">
-                                            <div>&nbsp;</div>
-                                        </div>
-                                    </div>}
+                                        :
+                                        <div>
+                                            <div className="row justify-content-center">
+                                                <img width="150"
+                                                     src={AnonymousPlayer}/>
+                                            </div>
+                                            <div className="row justify-content-center">
+                                                <div>&nbsp;</div>
+                                            </div>
+                                            <div className="row justify-content-center">
+                                                <div>&nbsp;</div>
+                                            </div>
+                                        </div>}
+                                </AnimateOnChange>
                             </div>
                         </div>
                     </div>
-                    <div className="col row justify-content-center align-items-center">
-                        <div className="">
+                    <div className="col container">
+                        <div>
+                            <div className="row mb-3 justify-content-center align-items-center">
+                                <Button variant="primary"
+                                        disabled={!this.props.myTeam.allowedToTransfer}
 
-                            <Button variant="primary"
-                                    disabled={!this.props.myTeam.allowedToTransfer}
+                                        onClick={this.props.myTeam.allowedToTransfer ? () => ManageTeamApi.sendTransferedPlayer(this.props.myTeam.transferablePlayers[this.props.myTeam.secondSelectedTransfer],
+                                            this.props.myTeam.myTeamForTransfer[this.props.myTeam.firstSelectedTransfer])
+                                            : null}>
+                                    <lottie-player
+                                        autoplay={!this.props.myTeam.allowedToTransfer}
+                                        loop
+                                        speed=".5"
+                                        mode="normal"
+                                        src="https://maxst.icons8.com/vue-static/landings/animated-icons/icons/left-right/left-right.json"
+                                    >
+                                    </lottie-player>
+                                </Button>
+                            </div>
+                            <div className="row mt-3 justify-content-center align-items-center">
+                                <Button variant="danger" onClick={() => {
+                                    this.props.selectFirstTransfer(null);
+                                    this.props.selectSecondTransfer(null);
+                                    this.props.enableTransferTable(false);
+                                    this.props.isAllowedToTransfer(false);
+                                }}>
 
-                                    onClick={this.props.myTeam.allowedToTransfer ? () => ManageTeamApi.sendTransferedPlayer(this.props.myTeam.transferablePlayers[this.props.myTeam.secondSelectedTransfer],
-                                        this.props.myTeam.myTeamForTransfer[this.props.myTeam.firstSelectedTransfer])
-                                        : null}>
-                                <lottie-player
-                                    autoplay={!this.props.myTeam.allowedToTransfer}
-                                    loop
-                                    speed=".5"
-                                    mode="normal"
-                                    src="https://maxst.icons8.com/vue-static/landings/animated-icons/icons/left-right/left-right.json"
-                                >
-                                </lottie-player>
-                            </Button></div>
+                                    <lottie-player
+                                        hover
+                                        loop
+                                        speed="1"
+                                        mode="normal"
+                                        src="https://maxst.icons8.com/vue-static/landings/animated-icons/icons/restart/restart.json"
+                                    >
+                                    </lottie-player>
+
+
+                                </Button>
+                            </div>
+                        </div>
+
                     </div>
                     <div className="col">
                         <div className="container">
                             <div className="row justify-content-center">
-                                {this.props.myTeam.secondSelectedTransfer || this.props.myTeam.secondSelectedTransfer === 0 ?
-                                    <div>
-                                        <div className="row justify-content-center">
-                                            <div><img width="150"
-                                                      src={this.props.myTeam.transferablePlayers[this.props.myTeam.secondSelectedTransfer].image}/>
+                                <AnimateOnChange
+                                    animationIn="bounceIn"
+                                    animationOut="bounceOut"
+                                >
+                                    {this.props.myTeam.secondSelectedTransfer || this.props.myTeam.secondSelectedTransfer === 0 ?
+                                        <div>
+                                            <div className="row justify-content-center">
+                                                <div><img width="150"
+                                                          src={this.props.myTeam.transferablePlayers[this.props.myTeam.secondSelectedTransfer].image}/>
+                                                </div>
+                                            </div>
+                                            <div className="row justify-content-center">
+                                                <div
+                                                    className="text-white">{this.props.myTeam.transferablePlayers[this.props.myTeam.secondSelectedTransfer].name}</div>
+                                            </div>
+                                            <div className="row justify-content-center">
+                                                <div
+                                                    className="text-white">{this.props.myTeam.transferablePlayers[this.props.myTeam.secondSelectedTransfer].position}</div>
                                             </div>
                                         </div>
-                                        <div className="row justify-content-center">
-                                            <div className="text-white">{this.props.myTeam.transferablePlayers[this.props.myTeam.secondSelectedTransfer].name}</div>
-                                        </div>
-                                        <div className="row justify-content-center">
-                                            <div className="text-white">{this.props.myTeam.transferablePlayers[this.props.myTeam.secondSelectedTransfer].position}</div>
-                                        </div>
-                                    </div>
-                                    :
-                                    <div>
-                                        <div className="row justify-content-center">
-                                            <img width="150"
-                                                 src={AnonymousPlayer}/>
-                                        </div>
-                                        <div className="row justify-content-center">
-                                            <div>&nbsp;</div>
-                                        </div>
-                                        <div className="row justify-content-center">
-                                            <div>&nbsp;</div>
-                                        </div>
-                                    </div>}
+                                        :
+                                        <div>
+                                            <div className="row justify-content-center">
+                                                <img width="150"
+                                                     src={AnonymousPlayer}/>
+                                            </div>
+                                            <div className="row justify-content-center">
+                                                <div>&nbsp;</div>
+                                            </div>
+                                            <div className="row justify-content-center">
+                                                <div>&nbsp;</div>
+                                            </div>
+                                        </div>}
+                                </AnimateOnChange>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-5">
+                <div className="row mb-5">
+                    <div className="col-md-5 col-xs-6">
                         <SquadPlayers/>
                     </div>
-                    <div className="col-2">
+                    <div className="col-md-2 hidden-xs hidden-sm">
                     </div>
-                    <div className="col-5">
+                    <div className="col-md-5 col-xs-6">
                         <AllPlayers/>
                     </div>
                 </div>
@@ -122,7 +166,12 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({}, dispatch)
+    return bindActionCreators({
+        selectSecondTransfer,
+        selectFirstTransfer,
+        enableTransferTable,
+        isAllowedToTransfer
+    }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Transfer);
