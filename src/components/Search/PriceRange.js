56f7e0952554695870474933f1c0a0d80aaa156a
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-11-06 19:49:47 +0330

    Add price range to components

---
+++ src/components/Search/PriceRange.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import { useDispatch, useSelector } from "react-redux";
+import {setPriceRange} from "../../_actions/searchActions"
+
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+export default function PriceRange() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState([2, 8]);
+  const dispatch = useDispatch();
+
+  const handleChange = (event, newValue) => {
+    // console.log("fucking range is",newValue)
+    setValue(newValue);
+    dispatch(setPriceRange(newValue));
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography id="range-slider" gutterBottom>
+        Price
+      </Typography>
+      <Slider
+        value={value}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        getAriaValueText={valuetext}
+        min={0}
+        max={10}
+        step={1}
+        marks
+      />
+    </div>
+  );
+}
