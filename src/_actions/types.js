707dc58d3dc7180042735e04487de13a60b15e03
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-11-06 18:04:20 +0330

    Add search action types to types.js

--- src/_actions/types.js
+++ src/_actions/types.js
@@ -6,3 +6,11 @@ export const auth_action_types = {
 export const picksquad_action_types = {
     SET_PLAYERS_SUCCESS : 'SET_PLAYERS_SUCCESS'
   }
+
+export const search_action_types = {
+    GET_PLAYERS_SUCCESS : 'GET_PLAYERS_SUCCESS',
+    GET_PLAYERS_FAILURE : 'GET_PLAYERS_FAILURE',
+    SET_PLAYERS_STATUS_SUCCESS : 'SET_PLAYERS_STATUS_SUCCESS',
+    SET_PLAYERS_CLUBS_SUCCESS : 'SET_PLAYERS_CLUBS_SUCCESS',
+
+  }
