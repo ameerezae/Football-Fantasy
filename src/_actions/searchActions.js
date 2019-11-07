e9c35a128d971e2f97ef5c6cf02aa90e985623ee
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-11-07 17:38:33 +0330

    Add getClubsRequest to actions

--- src/_actions/searchActions.js
+++ src/_actions/searchActions.js
@@ -7,13 +7,46 @@ export const playerSearchRequest = (credentials,name) => {
       dispatch(playerSearchSuccess(response))
   }
 }
+export const clubGetRequest = () => {
+  return async function(dispatch){
+    console.log("now i am requesting")
+      let response = await search_api.getClubs()
+      console.log("this is club response",response)
+      dispatch(clubGetSuccess(response))
+  }
+}
+
+// const filterByRange = (newRange,temp) => {
+//   for( var i = 0; i < temp.length; i++){ 
+//     if ( temp[i].price > newRange[1] && temp[i].price < newRange[0]) {
+//       temp.splice(i, 1); 
+//     }
+//  }
+// }
+
+// const clubsByRange = (clubs,temp,) => {
+//   let clubIds = [];
+//   for(var j = 0;j<;j++)
+//   for (var i = 0; i < temp.length; i++) { 
+//     if ( temp[i].club) {
+//       temp.splice(i, 1); 
+//     }
+//  }
+// }
 
 
 const playerSearchSuccess = players => (
   
   {
     type: acc.search_action_types.GET_PLAYERS_SUCCESS,
-    payload: players
+    payload: players.data,
+})
+
+const clubGetSuccess = clubs => (
+  
+  {
+    type: acc.search_action_types.GET_CLUBS_SUCCESS,
+    payload: clubs.data.clubs
 })
 
 export const setPlayerStatus = status => (
