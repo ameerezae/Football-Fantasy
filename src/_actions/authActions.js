c41a7dd0fc7763915209f5e02e7d1c422c5778ab
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-07 17:55:58 +0330

    Edit authActions

--- src/_actions/authActions.js
+++ src/_actions/authActions.js
@@ -5,7 +5,6 @@ import Login_page_api from "../_api/authApi"
 export const userSignInRequest = (credentials) => {
   return async function(dispatch){
       let response = await Login_page_api.login(credentials)
-      // console.log("this is response",response)
       localStorage.setItem("access_token", response.data.access_token)
       localStorage.setItem("refresh_token", response.data.refresh_token)
       dispatch(loginUserSuccess(response))
@@ -19,16 +18,16 @@ export const userSignUpRequest = (credentials) => {
       localStorage.setItem("refresh_token", response.data.refresh_token)
       if(response.message == "successful login")
       {
-      dispatch(loginUserSuccess(response))
+      dispatch(loginUserSuccess(response.data))
       }
       else{
-        dispatch(loginUserFailure(response))
+        dispatch(loginUserFailure(response.data))
       }
   }
 }
 
 const loginUserSuccess = userObj => (
-  
+
   {
     type: acc.auth_action_types.LOGIN_SUCCESS,
     payload: userObj,
