d2920888d4ec954aa13bb40ae225338c41803232
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-07 17:53:19 +0330

    Edit authReducer

--- src/_reducers/authReducer.js
+++ src/_reducers/authReducer.js
@@ -3,16 +3,13 @@ import  * as  acc from "../_actions/types"
 const initialState = {
     // currentUser: {},
     logged_in: false,
-    message : '',
   }
 
 export function authReducer(state = initialState, action) {
         switch (action.type) {
             case acc.auth_action_types.LOGIN_SUCCESS:
-                console.log("user info :",action.payload)
                 return {...state,message: action.payload.data.message,logged_in : action.logged_in}
             case acc.auth_action_types.LOGIN_FAILURE:
-                console.log(action.payload.message)
                 return {...state, message: action.payload.message,logged_in : action.logged_in}
             default:
             return state;
