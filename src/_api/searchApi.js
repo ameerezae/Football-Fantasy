d0eb7d3726e5e9742b86ca8480aad3cf4c299131
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-11-07 17:43:52 +0330

    Add getClubs request

--- src/_api/searchApi.js
+++ src/_api/searchApi.js
@@ -13,11 +13,24 @@ class search_api {
             "Authorization" : `Bearer ${token}`
             }
         }
-        let response = await axios.post(api_urls.PICKSQUAD,JSON.stringify(
+        let response = await axios.get(api_urls.PLAYERS,config)
+        console.log("response player",response)
+        return response  
+    }
+    static async getClubs(){
+        console.log("hi we are in getCLubs")
+
+        const token = localStorage.getItem("access_token")
+        const config = 
+        {
+            mode: "cors",
+            headers: 
             {
-                
+            'Content-Type': 'application/json',
+            "Authorization" : `Bearer ${token}`
             }
-        ),config)
+        }
+        let response = await axios.get(api_urls.CLUBS,config)
         console.log("response",response)
         return response  
     }
