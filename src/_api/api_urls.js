730cede9db6d1ecba3aeef2f88c6e0ac6a04757d
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-11-07 17:41:35 +0330

    Add clubs ,players api

--- src/_api/api_urls.js
+++ src/_api/api_urls.js
@@ -2,3 +2,6 @@ export const MAIN = "http://172.17.3.123:5000"
 export const LOGIN = MAIN + "/auth/login"
 export const SIGNUP = MAIN + "/auth/registeration"
 export const PICKSQUAD = MAIN + "/team/pick-squad"
+export const CLUBS = MAIN + "/club/2021/clubs"
+export const PLAYERS = MAIN + "/player/2021/pick-squad"
+
