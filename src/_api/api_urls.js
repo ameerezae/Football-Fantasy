358eb93bd12dd40d85a6bec46f2a38a068d6f4e8
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-11-08 19:04:13 +0330

    Add get squad api to api_urls

--- src/_api/api_urls.js
+++ src/_api/api_urls.js
@@ -2,6 +2,7 @@ export const MAIN = "http://172.17.3.123:5000"
 export const LOGIN = MAIN + "/auth/login"
 export const SIGNUP = MAIN + "/auth/registeration"
 export const PICKSQUAD = MAIN + "/team/pick-squad"
+export const MANAGE_TEAM = MAIN + "/team/my-team";
 export const CLUBS = MAIN + "/club/2021/clubs"
 export const PLAYERS = MAIN + "/player/2021/pick-squad"
 
