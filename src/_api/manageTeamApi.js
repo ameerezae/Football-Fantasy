2eee052e1413c2cc87a68eed57b054f850501448
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-11-11 02:44:04 +0330

    Edit sendSubsTeam action

--- src/_api/manageTeamApi.js
+++ src/_api/manageTeamApi.js
@@ -38,30 +38,38 @@ class ManageTeamApi {
     }
 
     static async sendSubsTeam (team,captain){
-        const token = localStorage.getItem("access_token");
-        const config =
-            {
-                mode: "cors",
-                headers:
-                    {
-                        'Content-Type': 'application/json',
-                        "Authorization": `Bearer ${token}`
-                    }
-            };
-        const thisBody = {
-            "squad" : team,
-            "captain-id" : captain,
+        try{
+            const token = localStorage.getItem("access_token");
+            const config =
+                {
+                    mode: "cors",
+                    headers:
+                        {
+                            'Content-Type': 'application/json',
+                            "Authorization": `Bearer ${token}`
+                        }
+                };
+            const thisBody = {
+                "squad" : team,
+                "captain-id" : captain,
+            }
+            const body = JSON.stringify(
+                thisBody
+            );
+            const response = await axios.put(
+                api_urls.MANAGE_TEAM,
+                body,
+                config
+            );
+
+            return response;
+        }catch (e) {
+            if(e.response){
+
+                return e.response;
+            }
         }
-        const body = JSON.stringify(
-            thisBody
-        );
-        console.log(thisBody,"Bodyyyyyyy");
-        const response = axios.put(
-            api_urls.MANAGE_TEAM,
-            body,
-            config
-        );
-        return response;
+
     }
 
     static async sendTransferedPlayer(playerIn, playerOut) {
