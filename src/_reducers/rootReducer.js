fcc10cacff9fb0b1c58f1ded5c17594c31596a8f
amirrezaeii <amir.rezaei1999@yahoo.com>
2019-11-30 17:49:27 +0330

    Add dashboardReducer to rootReducer

--- src/_reducers/rootReducer.js
+++ src/_reducers/rootReducer.js
@@ -3,12 +3,14 @@ import formatReducer from "./formatReducer";
 import { authReducer } from './authReducer';
 import manageTeamReaducer from "./manageTeamReaducer";
 import {searchReducer} from "./searchReducer"
+import {dashboardReducer} from "./dashboardReducer";
 
 const rootReducer = combineReducers({
     authReducer,
     formatReducer,
     searchReducer,
     manageTeamReaducer,
+    dashboardReducer,
 });
 
 export default rootReducer;
