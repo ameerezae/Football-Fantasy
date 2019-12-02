2316ba9167564b7003a11349e080898324e29654
amirrezaeii <amir.rezaei1999@yahoo.com>
2019-12-02 14:34:47 +0330

    Edit competitions and competition return

--- src/_containers/dashboard/competitions/competiton/competition.js
+++ src/_containers/dashboard/competitions/competiton/competition.js
@@ -4,10 +4,10 @@ import {Carousel} from "react-bootstrap";
 const Competition = (props) => {
     return (
         <Carousel.Item>
-            <img className="d-block w-100" src={props.image} alt="competition"/>
+            <img className="d-block w-100" src={"https://amp.computerbild.de/imgs/1/1/1/2/4/2/8/3/Fussball-Bundesliga-658x370-53d2c834c2e290a0.jpg"} alt="competition"/>
             <Carousel.Caption>
                 <h3>{props.name}</h3>
-                <p>{props.area.name}</p>
+                <p>{props.area_name}</p>
             </Carousel.Caption>
         </Carousel.Item>
     );
