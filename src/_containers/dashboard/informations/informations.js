1cea120337cb969fbcb347197cbe525c4a256921
amir <amir.rezaei1999@yahoo.com>
2019-12-06 19:17:24 +0330

    Create Information class

---
+++ src/_containers/dashboard/informations/informations.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Information extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Information;
