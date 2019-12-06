cb303e1c9395e1e10cd7f9d33fb31c2b9ca90ecd
amir <amir.rezaei1999@yahoo.com>
2019-12-06 19:15:03 +0330

    Add information class to dashboard

--- src/_containers/dashboard/dashboard/dashboard.js
+++ src/_containers/dashboard/dashboard/dashboard.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 import Competitions from "../competitions/competitions";
+import Information from "../informations/informations";
 class Dashboard extends Component {
     render() {
         return (
-            <Competitions/>
+            <div>
+                <Information/>
+                <Competitions/>
+            </div>
         );
     }
 }
