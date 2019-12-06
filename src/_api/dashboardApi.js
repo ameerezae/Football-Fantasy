f1058c1e74dc98d7e9f4c78cd2c9d48c3a316835
amir <amir.rezaei1999@yahoo.com>
2019-12-06 19:14:21 +0330

    Add getUserInformation api

--- src/_api/dashboardApi.js
+++ src/_api/dashboardApi.js
@@ -14,6 +14,19 @@ class DashboardApis{
             if(e.response) return e.response;
         }
     }
+
+    static async getUserInformation () {
+        try {
+            const response = await axios.get(
+                api_urls.INFORMATION,
+                universal_constants.CONFIG_WITH_AUTH
+            );
+            if(response) return response;
+        }catch (e) {
+            if(e.response) return e.response;
+        }
+    }
+
 }
 
 export default DashboardApis;
