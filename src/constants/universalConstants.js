44fc8ce4538928d998169b51015cd79bfda7e56c
amir <amir.rezaei1999@yahoo.com>
2019-12-06 19:16:47 +0330

    Add requests status codes

--- src/constants/universalConstants.js
+++ src/constants/universalConstants.js
@@ -1,4 +1,9 @@
 export const ACCESS_TOKEN = "access_token";
+export const REQUESTS_STATUS = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+};
 
 export const CONFIG_WITH_AUTH = {
     mode: "cors",
