50658e47e5c748fbc825370aa073d59990625906
amir <amir.rezaei1999@yahoo.com>
2019-12-06 19:15:58 +0330

    Add dashboard information constants

--- src/constants/dashboard/dashboardConstants.js
+++ src/constants/dashboard/dashboardConstants.js
@@ -1,1 +1,31 @@
 export const COMPETITIONS = "competitions";
+
+
+export const INFORMATION_CONSTANTS = {
+    USER: "user",
+    EMAIL: "email",
+    USER_NAME: "username",
+    SQUADS : "squads",
+    BUDGET : "budget",
+    CAPTAIN : "captain",
+    CAPTAIN_IMAGE : "image",
+    CAPTAIN_NAME : "name",
+    CAPTAIN_POINT : "point",
+    CAPTAIN_POSITION: "position",
+    CAPTAIN_PRICE : "price",
+    CAPTAIN_SHIRT: "shirt_number",
+    CAPTAIN_STATUS: "status",
+    COMPETITION: "competition",
+    COMPETITION_AREA : "area",
+    COMPETITION_AREA_ID : "id",
+    COMPETITION_AREA_NAME : "name",
+    COMPETITION_END_DATE: "endDate",
+    COMPETITION_ID: "id",
+    COMPETITION_IMAGE: "image",
+    COMPETITION_LAST_UPDATE: "lastUpdate",
+    COMPETITION_NAME : "name",
+    COMPETITION_START_DATE : "startDate",
+    SQUAD_NAME : "name",
+    SQUAD_POINT : "point",
+
+};
