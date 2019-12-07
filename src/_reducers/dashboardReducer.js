3d439d1a3de33ddcc7a997c4f5bda4b10b609c18
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-12-07 23:43:40 +0330

    Add set competition action to dashboard reducer

--- src/_reducers/dashboardReducer.js
+++ src/_reducers/dashboardReducer.js
@@ -2,15 +2,19 @@ import * as types from "../_actions/types";
 
 const initialState = {
     areCompetitionsFetched : false,
+    competitions: [],
+    selectedCompetition: null,
 }
 
 
 export function dashboardReducer (state = initialState,action){
     switch(action.type){
         case types.dashboard_action_types.GET_COMPETITIONS_SUCCESS : {
-            return {...state, competitions : action.payload, areCompetitionsFetched: true};
+            return {...state, competitions : action.payload, selectedCompetition : action.payload[0], areCompetitionsFetched: true};
         }
-
+        case types.dashboard_action_types.GET_CURRENT_COMPETITION_SUCCESS : {
+            return {...state, selectedCompetition : action.payload};
+        }
         default : return state;
     }
 }
