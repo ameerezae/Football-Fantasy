29f99a70b57d6a75f1b0e12591b703762781bef3
amir <amir.rezaei1999@yahoo.com>
2019-12-11 01:35:23 +0330

    Add statistics reducer

---
+++ src/_reducers/statisticsReducer.js
@@ -0,0 +1,13 @@
+import * as types from "../_actions/types";
+
+const initialState = {
+
+};
+
+export function statisticsReducer(state = initialState, action) {
+    switch (action.type) {
+
+        default: return state;
+    }
+
+}
