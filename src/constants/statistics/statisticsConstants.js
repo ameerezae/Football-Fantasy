a272880bd29a208b7885ac26340b97eb8aeece51
amir <amir.rezaei1999@yahoo.com>
2019-12-11 23:15:23 +0330

    Add onePlayer statistics constants

---
+++ src/constants/statistics/statisticsConstants.js
@@ -0,0 +1,16 @@
+export const ONE_PLAYER_STATISTICS = {
+    PLAYER : "player",
+    POINT : "point",
+    NAME : "name",
+    POSITION : "position",
+    STATUS : "status",
+    PRICE : "price",
+    CLUB : "club",
+    CLUB_NAME : "name",
+    CLUB_IMAGE : "image",
+    EVENTS : "events",
+    EVENT_TYPE : "event_type",
+    MATCH_ID : "match",
+    EVENT_OPPONENT_NAME : "Opponent_name",
+    EVENT_MINUTE : "minute",
+};
