341baedf92e4784bb55fe31fc6251110d646d8b6
amir <amir.rezaei1999@yahoo.com>
2019-12-11 23:18:23 +0330

    Add onePlayerStatistics state

--- src/_reducers/statisticsReducer.js
+++ src/_reducers/statisticsReducer.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export function statisticsReducer(state = initialState, action) {
     switch (action.type) {
-
+        case types.statistics_action_types.GET_PLAYER_STATISTICS_SUCCESS : return {...state, playerStatistics : action.payload};
         default: return state;
     }
 
