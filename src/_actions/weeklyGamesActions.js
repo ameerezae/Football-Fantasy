25c7b3a1855aee6ab016ebd52ab7bce386c6fb9f
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-12-13 00:40:29 +0330

    Add constants to weeklyActions and handle get game failure action dispatch

--- src/_actions/weeklyGamesActions.js
+++ src/_actions/weeklyGamesActions.js
@@ -1,12 +1,13 @@
 import * as types from "./types";
 import weekly_game_api from "../_api/weeklyGameAPi"
-
+import * as gameConstants from '../constants/games/gamesConstants'
 
 export const getGames = () => {
     return async function (dispatch) {
         const response = await weekly_game_api.getGames();
+        console.log("games response" , response)
         // if (response.data[dashboardConstants.COMPETITIONS]) dispatch(dispatchGetGames(response.data[dashboardConstants.COMPETITIONS]))
-        if (response.data) dispatch(dispatchGetGamesSuccess(response.data))
+        if (response.length !== 0) dispatch(dispatchGetGamesSuccess(response.data[gameConstants.LIST_SUBJECT.GAMES]))
         else dispatch(dispatchGetGamesFailure())
     }
 }
@@ -15,13 +16,12 @@ const dispatchGetGamesSuccess = (games) => (
     {
         type: types.weeklygames_action_types.GET_GAMES_SUCCESS,
         payload: games,
-        games_fetched = true,
+        games_fetched: true,
     }
 );
 const dispatchGetGamesFailure = () => (
     {
         type: types.weeklygames_action_types.GET_GAMES_FAILURE,
-        payload: games,
-        games_fetched = false,
+        games_fetched: false,
     }
 );
