0fe7aaa4d9ca1afb9bceaefc2a7e671500e25913
amir <amir.rezaei1999@yahoo.com>
2019-12-13 14:19:44 +0330

    Add arePlayerStatisticsFetched to the reducer

--- src/_reducers/statisticsReducer.js
+++ src/_reducers/statisticsReducer.js
@@ -1,12 +1,12 @@
 import * as types from "../_actions/types";
 
 const initialState = {
-
+    arePlayerStatisticsFetched : false,
 };
 
 export function statisticsReducer(state = initialState, action) {
     switch (action.type) {
-        case types.statistics_action_types.GET_PLAYER_STATISTICS_SUCCESS : return {...state, playerStatistics : action.payload};
+        case types.statistics_action_types.GET_PLAYER_STATISTICS_SUCCESS : return {...state, playerStatistics : action.payload, arePlayerStatisticsFetched: true};
         default: return state;
     }
 
