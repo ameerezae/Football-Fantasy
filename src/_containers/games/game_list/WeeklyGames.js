940e107693befc6462f6f4f6ef866a2a38f8d7af
amir <amir.rezaei1999@yahoo.com>
2019-12-20 01:18:46 +0330

    Add preLoader and no match field to weekly games

--- src/_containers/games/game_list/WeeklyGames.js
+++ src/_containers/games/game_list/WeeklyGames.js
@@ -1,81 +1,77 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import {getGames} from '../../../_actions/weeklyGamesActions'
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
+import {Alert,Spinner} from "react-bootstrap";
 
 class WeeklyGames extends Component {
     componentWillMount() {
         this.props.getGames();
     }
-	render() {
-		return (
-			<div>
-                <ListGroup>
-                {this.props.games.games_fetched ?
-                    // <ListGroup variant="info">
-                    //     <ListGroup.Item variant="dark">Cras justo odio</ListGroup.Item>
-                    //     <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                    //     <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                    //     <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                    // </ListGroup> 
-                    this.props.games.games.map((element, key) => {
-                        console.log(element)
-                        return (
-                            // <ListItem key={key} className="text-white"
-                            //           disabled={!this.props.myTeam.enableTable}>
-                            //     <ListItemGraphic className="list-image"
-                            //                      graphic={<img src={element.image} alt="sd"/>}/>
-                            //     <ListItemText
-                            //         className="text-white"
-                            //         primaryText={element.name.slice(0, 10)}
-                            //         secondaryText={element.club}/>
-                            //     <ListItemText
-                            //         className="whiteText ml-3"
-                            //         primaryText={element.price}
-                            //         secondaryText={element.position}/>
-                            //     <ListItemMeta
-                            //         style={{color: "white", fontSize: "1.5rem", verticalAlign: "center"}}
-                            //         meta={<IoIosInformationCircle/>}/>
-                            // </ListItem>
-                            <ListGroup.Item variant='dark'>
-                                <div style={{display: 'table-row'}}>
-                                    <div style={{display: 'table-cell',width: '50%'}}>
-                                        <div style={{display: 'table-row'}}>   
-                                            <div style={{width: '20%',display: 'table-cell',align: "left"}}>
-                                                <img src={element.homeTeam.image} alt="sd" style={{height:'auto',width:'50%'}}/>
+
+    render() {
+        return (
+            <div>
+                {this.props.games.games_fetched_success ?
+                    <ListGroup>
+                        {this.props.games.games.map((element, key) => {
+                            console.log(element)
+                            return (
+                                <ListGroup.Item variant='dark'>
+                                    <div style={{display: 'table-row'}}>
+                                        <div style={{display: 'table-cell', width: '50%'}}>
+                                            <div style={{display: 'table-row'}}>
+                                                <div style={{width: '20%', display: 'table-cell', align: "left"}}>
+                                                    <img src={element.homeTeam.image} alt="sd"
+                                                         style={{height: 'auto', width: '50%'}}/>
+                                                </div>
+                                                <div style={{width: '40%', display: 'table-cell', align: "left"}}>
+                                                    <h5>{element.homeTeam.name}</h5>
+                                                </div>
+                                                <div style={{width: '40%', display: 'table-cell', align: "left"}}>
+                                                    <h5>{element.homeTeamScore}</h5>
+                                                </div>
                                             </div>
-                                            <div style={{width: '40%',display: 'table-cell',align: "left"}}>
-                                                <h5>{element.homeTeam.name}</h5>
-                                            </div>
-                                            <div style={{width: '40%',display: 'table-cell',align: "left"}}>
-                                                <h5>{element.homeTeamScore}</h5>
+                                        </div>
+                                        <div style={{display: 'table-cell', width: '50%'}}>
+                                            <div style={{display: 'table-row'}}>
+                                                <div style={{width: '40%', display: 'table-cell', align: "right"}}>
+                                                    <h5>{element.awayTeamScore}</h5>
+                                                </div>
+                                                <div style={{width: '40%', display: 'table-cell', align: "right"}}>
+                                                    <h5>{element.awayTeam.name}</h5>
+                                                </div>
+                                                <div style={{width: '20%', display: 'table-cell', align: "right"}}>
+                                                    <img src={element.awayTeam.image} alt="sd"
+                                                         style={{height: 'auto', width: '50%'}}/>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div style={{display: 'table-cell',width: '50%'}}>
-                                        <div style={{display: 'table-row'}}>  
-                                            <div style={{width: '40%',display: 'table-cell',align: "right"}}>
-                                                <h5>{element.awayTeamScore}</h5>
-                                            </div>
-                                            <div style={{width: '40%',display: 'table-cell',align: "right"}}>
-                                                <h5>{element.awayTeam.name}</h5>
-                                            </div>
-                                            <div style={{width: '20%',display: 'table-cell',align: "right"}}>
-                                                <img src={element.awayTeam.image} alt="sd" style={{height:'auto',width:'50%'}}/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </ListGroup.Item>
-                        )
-                    })
-                            : null } 
-                </ListGroup> 
-			</div>
-		)
-	}
+                                </ListGroup.Item>
+                            )
+                        })}
+                    </ListGroup> :
+                    this.props.games.games_fetched_failed ?
+
+                    <Alert variant="success">
+                        <Alert.Heading>No recent match!!!</Alert.Heading>
+                        <p>
+                            There are no recent matches, Come back later
+                        </p>
+                        <hr/>
+                        <p className="mb-0">
+                            Whenever you need to, be sure to use margin utilities to keep things nice
+                            and tidy.
+                        </p>
+                    </Alert> : <Spinner animation="border" variant="danger" />
+                }
+            </div>
+        )
+    }
 }
+
 function mapStateToProps(state) {
     return {
         games: state.gameReducer
