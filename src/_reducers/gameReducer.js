cca83c0eb516fa234094924a6888629ad00f47bd
amir <amir.rezaei1999@yahoo.com>
2019-12-20 01:19:32 +0330

    Edit gameReducer function and initialState

--- src/_reducers/gameReducer.js
+++ src/_reducers/gameReducer.js
@@ -1,15 +1,16 @@
 import  * as  acc from "../_actions/types"
 const initialState = {
     games: [],
-    games_fetched: false,
+    games_fetched_success: false,
+    games_fetched_failed: false,
   }
 
 export function gameReducer(state = initialState, action) {
         switch (action.type) {
             case acc.weeklygames_action_types.GET_GAMES_SUCCESS:
-                return {...state, games: action.payload, games_fetched : action.games_fetched = true}
+                return {...state, games: action.payload, games_fetched_success : action.games_fetched = true}
             case acc.weeklygames_action_types.GET_GAMES_FAILURE:
-                return {...state, games_fetched:action.games_fetched}
+                return {...state, games_fetched_failed:action.games_fetched};
             default:
             return state;
         }
