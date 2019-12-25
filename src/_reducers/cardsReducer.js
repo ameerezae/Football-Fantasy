9c5f1e2c4c60d392845939e55179004e58f3cd5f
amir <amir.rezaei1999@yahoo.com>
2019-12-26 00:13:07 +0330

    Add cardsRducer function

---
+++ src/_reducers/cardsReducer.js
@@ -0,0 +1,16 @@
+import * as types from "../_actions/types";
+
+
+const initialState = {
+
+}
+
+
+export function cardsReducer(state = initialState, action){
+    switch (action.type) {
+
+        
+        default : return state;
+
+    }
+}
