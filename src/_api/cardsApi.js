b0a8d790286d14470de52b555bf8df881ddb829e
amir <amir.rezaei1999@yahoo.com>
2019-12-26 00:23:37 +0330

    Add getAllCards api to cardsApi

---
+++ src/_api/cardsApi.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import * as api_urls from "./api_urls";
+import url_handler from "./url_handler";
+import * as universalConstants from "../constants/universalConstants";
+
+
+class CardsApi {
+    static async getAllCards(){
+        const url = url_handler.competition_handler(api_urls.CARDS_START, api_urls.CARDS_END);
+        try {
+            const respone = await axios.get(
+                url,
+                universalConstants.CONFIG_WITH_AUTH
+            )
+            if(respone) return respone;
+        }catch (e) {
+            if(e.response) return e.response;
+        }
+    }
+
+
+
+}
+
+export default CardsApi;
