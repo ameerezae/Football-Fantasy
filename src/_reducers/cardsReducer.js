a4b562f56d37040425fb19bcd4b0fc91f658931a
amir <amir.rezaei1999@yahoo.com>
2019-12-26 00:44:18 +0330

    Add get cards success and failed to cases

--- src/_reducers/cardsReducer.js
+++ src/_reducers/cardsReducer.js
@@ -2,14 +2,22 @@ import * as types from "../_actions/types";
 
 
 const initialState = {
-
+    fetchedCards : false,
 }
 
 
 export function cardsReducer(state = initialState, action){
     switch (action.type) {
+        case types.cards_action_types.GET_ALL_CARDS_SUCCESS :return {
+            ...state,
+            cards : action.payload,
+            fetchedCards: true,
+        };
+        case types.cards_action_types.GET_ALL_CARDS_FAILED : return {
+            ...state,
+            cardsFetchError : action.payload,
+        };
 
-        
         default : return state;
 
     }
