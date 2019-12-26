68f3cd1e45305c3d2ec8e8e62a4174321f96d426
amir <amir.rezaei1999@yahoo.com>
2019-12-27 01:33:43 +0330

    Add postCard api

--- src/_api/cardsApi.js
+++ src/_api/cardsApi.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import * as api_urls from "./api_urls";
 import url_handler from "./url_handler";
 import * as universalConstants from "../constants/universalConstants";
+import * as cardsConstants from "../constants/cards/cardsConstants";
+import {CARDS_POST} from "../constants/cards/cardsConstants";
 
 
 class CardsApi {
@@ -13,6 +15,25 @@ class CardsApi {
                 universalConstants.CONFIG_WITH_AUTH
             )
             if(respone) return respone;
+        }catch (e) {
+            if(e.response) return e.response;
+        }
+    }
+
+    static async postCard(name ,mode){
+        const url = url_handler.competition_handler(api_urls.CARDS_START, api_urls.CARDS_END);
+        try {
+            const response = await axios.post(
+                url,
+                JSON.stringify(
+                    {
+                        name : name,
+                        mode : mode
+                    }
+                ),
+                universalConstants.CONFIG_WITH_AUTH
+            );
+            if(response) return response;
         }catch (e) {
             if(e.response) return e.response;
         }
