f31cc5169afb4954425c0da58e1d5ba57988250a
amir <amir.rezaei1999@yahoo.com>
2019-12-27 12:37:30 +0330

    Edit postCard action

--- src/_actions/cardsActions.js
+++ src/_actions/cardsActions.js
@@ -15,3 +15,17 @@ export const getAllCards = () => {
 
 const dispatchGetAllCards = (cards) => ({type : types.cards_action_types.GET_ALL_CARDS_SUCCESS, payload : cards});
 const dispatchGetAllCardsFailed = (message) => ({type : types.cards_action_types.GET_ALL_CARDS_FAILED, payload : message});
+
+export const postCard = (name,mode) => {
+    return async function(dispatch){
+        const res = await CardsApi.postCard(name, mode);
+
+        if(res.status === universal_constants.REQUESTS_STATUS.OK){
+            const response = await CardsApi.getAllCards() ;
+            if(response.status === universal_constants.REQUESTS_STATUS.OK){
+                dispatch(dispatchGetAllCards(response.data));
+            }
+            else dispatch(dispatchGetAllCardsFailed(response.data[cardsConstants.MESSAGE]));
+        }
+    }
+}
