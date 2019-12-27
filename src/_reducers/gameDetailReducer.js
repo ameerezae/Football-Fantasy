03d88aaa9cb00e84f6a4c1515559e0b2329b96ee
Hamidreza Sanaee <shrsanaee@gmail.com>
2019-12-27 18:13:02 +0330

    Modify gameDetailReducer and add clear reducer

--- src/_reducers/gameDetailReducer.js
+++ src/_reducers/gameDetailReducer.js
@@ -10,7 +10,7 @@ export function gameDetailReducer(state = initialState, action) {
                 return {...state, game_details: action.payload, game_details_fetched : action.game_details_fetched}
             case acc.gamedetail_action_types.GET_GAME_DETAIL_FAILURE:
                 return {...state, game_details_fetched:action.game_details_fetched};
-            case acc.gamedetail_action_types.CLEAR_REDUCER_SUCCESS :
+            case acc.gamedetail_action_types.GAME_DETAIL_CLEAR_REDUCER_SUCCESS :
                 return {game_details: [],game_details_fetched: false};
             default:
             return state;
