9c3fb22b6862287147e3660828b5895240565728
amir <amir.rezaei1999@yahoo.com>
2019-12-27 12:39:21 +0330

    Edit card ui

--- src/_containers/manageTeam/substitution/Cards/card.js
+++ src/_containers/manageTeam/substitution/Cards/card.js
@@ -1,10 +1,50 @@
 import React, {Component} from 'react';
+import ReactCardFlipper from "react-card-flipper";
+import ReactCardFlip from 'react-card-flip';
+import {Button} from "react-bootstrap";
+import * as cardsConstants from "../../../../constants/cards/cardsConstants";
 
 class Card extends Component {
+    state = {
+        isFlipped: false
+    };
+    toggle = () => {
+        this.setState({isFlipped : !this.state.isFlipped})
+    }
     render() {
         return (
-            <div className="container bg-white">
-                {this.props.name}
+            <div className="">
+                <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+                    <div>
+                        <div className={this.props.mode === cardsConstants.CARDS_MODES.INACTIVE? "card bg-c-pink order-card" : "card bg-c-green order-card" }>
+                            <div className="card-block">
+                                <h6 className="m-b-20">{this.props.name}</h6>
+                                <h2 className="text-right"><i className="fa fa-rocket f-left"></i><span>486</span></h2>
+                                <p className="m-b-0">{this.props.mode}<span className="f-right">351</span></p>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <Button variant="primary" onClick={()=>this.toggle()}>
+                                {this.props.mode === cardsConstants.CARDS_MODES.INACTIVE ? cardsConstants.CARDS_MODES.ACTIVE : cardsConstants.CARDS_MODES.INACTIVE}
+                            </Button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className={this.props.mode === cardsConstants.CARDS_MODES.INACTIVE? "card bg-c-pink order-card" : "card bg-c-green order-card" }>
+                            <div className="card-block">
+                                <h6 className="m-b-20">{this.props.name}</h6>
+                                <h2 className="text-right"><i className="fa fa-credit-card f-left"></i><span>486</span></h2>
+                                <p className="m-b-0">{this.props.mode}<span className="f-right">351</span></p>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <Button variant="primary" onClick={()=>this.toggle()}>
+                                {this.props.mode === cardsConstants.CARDS_MODES.INACTIVE ? cardsConstants.CARDS_MODES.ACTIVE : cardsConstants.CARDS_MODES.INACTIVE}
+                            </Button>
+                        </div>
+                    </div>
+                </ReactCardFlip>
             </div>
         );
     }
