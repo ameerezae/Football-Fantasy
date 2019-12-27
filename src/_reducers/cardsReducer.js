aabf2211e481cc038317abcfaa98487306b918ae
amir <amir.rezaei1999@yahoo.com>
2019-12-27 13:57:11 +0330

    Add find active card to the cases

--- src/_reducers/cardsReducer.js
+++ src/_reducers/cardsReducer.js
@@ -3,7 +3,8 @@ import * as types from "../_actions/types";
 
 const initialState = {
     fetchedCards : false,
-}
+    activeCard : "nothing",
+};
 
 
 export function cardsReducer(state = initialState, action){
@@ -16,6 +17,10 @@ export function cardsReducer(state = initialState, action){
         case types.cards_action_types.GET_ALL_CARDS_FAILED : return {
             ...state,
             cardsFetchError : action.payload,
+        };
+        case types.cards_action_types.FIND_ACTIVE_CARD_SUCCESS : return {
+            ...state,
+            activeCard: action.payload,
         };
 
         default : return state;
