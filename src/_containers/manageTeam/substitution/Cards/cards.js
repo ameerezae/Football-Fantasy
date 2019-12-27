7f266a88d6b03367a17f63fba65234ec3b557897
amir <amir.rezaei1999@yahoo.com>
2019-12-27 12:43:10 +0330

    Edit mapping of the cards

--- src/_containers/manageTeam/substitution/Cards/cards.js
+++ src/_containers/manageTeam/substitution/Cards/cards.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import Card from "./card";
-import {getAllCards} from "../../../../_actions/cardsActions";
+import "./Cards.scss";
+import {getAllCards, postCard} from "../../../../_actions/cardsActions";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
+import * as cardsConstants from "../../../../constants/cards/cardsConstants";
+
 class Cards extends Component {
     componentWillMount() {
         this.props.getAllCards();
@@ -12,28 +15,38 @@ class Cards extends Component {
         return (
             <div>
                 {this.props.cardsState.fetchedCards ?
-                Object.keys(this.props.cardsState.cards).map((key)=>{
-                    return(
-                        <div className="row">
-                            <Card name = {key}/>
-                        </div>
-                    )
-                }) : null};
+                    <div className="row">
+
+                        {Object.keys(this.props.cardsState.cards).map((key) => {
+                                return (
+                                    <div className="col-3" onClick={() => {
+                                        this.props.postCard(key, this.props.cardsState.cards[key] === cardsConstants.CARDS_MODES.ACTIVE ? cardsConstants.CARDS_MODES.INACTIVE : cardsConstants.CARDS_MODES.ACTIVE);
+                                    }}>
+                                        <Card name={key} mode={this.props.cardsState.cards[key]}/>
+                                    </div>
+                                )
+
+                            }
+                        )}
+                    </div>
+                    : null};
             </div>
         );
     }
 }
 
 
-function mapStateToProps(state){
-    return{
-        cardsState : state.cardsReducer,
+function mapStateToProps(state) {
+    return {
+        cardsState: state.cardsReducer,
     }
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
     return bindActionCreators({
         getAllCards,
-    },dispatch)
+        postCard
+    }, dispatch)
 }
-export default connect(mapStateToProps,mapDispatchToProps)(Cards);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cards);
