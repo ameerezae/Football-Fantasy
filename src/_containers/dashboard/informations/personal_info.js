96bbd6075d27a9cdfb2d9755d998a85182ea05ed
amirrezaeii <amir.rezaei1999@yahoo.com>
2020-04-16 19:49:58 +0430

    Edit log out button

--- src/_containers/dashboard/informations/personal_info.js
+++ src/_containers/dashboard/informations/personal_info.js
@@ -3,6 +3,9 @@ import {connect} from "react-redux";
 import {Button} from "react-bootstrap";
 import * as dashboardConstants from "../../../constants/dashboard/dashboardConstants";
 import Edit from "./Edit";
+import *as universalCons from "../../../constants/universalConstants";
+
+
 class PersonalInfo extends Component {
     render() {
         return (
@@ -35,9 +38,15 @@ class PersonalInfo extends Component {
                                             </div>
                                             <div className="row mt-2">
                                                 <div className="col-3   whiteText text-center"><Edit/></div>
-                                                <div className="col-9  whiteText text-center"><Button href={"/"} variant="danger" onClick={()=>{
-                                                    localStorage.removeItem("access-token");
-                                                }}>LOGOUT</Button></div>
+                                                <div className="col-9  whiteText text-center">
+                                                        <Button variant="danger" href={"/"} onClick={()=>{
+                                                            localStorage.removeItem(universalCons.ACCESS_TOKEN);
+                                                            localStorage.removeItem(universalCons.REFRESH_TOKEN);
+                                                            localStorage.removeItem(universalCons.CURRENT_COMP);
+                                                        }}>
+                                                            LOGOUT
+                                                        </Button>
+                                                </div>
                                             </div>
                                         </div>
 
